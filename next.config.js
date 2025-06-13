f47766a546de22342064f576212056073cbb185a
oziomoluabi <ozi100@outlook.com>
2025-06-13 06:15:17 +0100

    ..

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  basePath: '/<repository-name>', // Replace <repository-name> with your GitHub repo name
+  images: {
+    unoptimized: true, // Disable server-side image optimization
+  },
 };
 
 module.exports = nextConfig;
